a55607438c8586b5d12d93fbfc589ae6aa86fe41
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-12-13 19:01:20 +0000

    cambiosVistas

---
+++ src/main/java/com/example/aerolineascanary7islands/controllers/VuelosController.java
@@ -0,0 +1,4 @@
+package com.example.aerolineascanary7islands.controllers;
+
+public class VuelosController {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,14 +4,13 @@ module com.example.aerolineascanary7islands {
     requires java.persistence;
     requires java.sql;
 
-
     requires org.hibernate.commons.annotations;
     requires org.hibernate.orm.core;
 
     opens com.example.aerolineascanary7islands to javafx.fxml;
     exports com.example.aerolineascanary7islands;
     exports com.example.aerolineascanary7islands.controllers;
+    opens com.example.aerolineascanary7islands.controllers to javafx.fxml;
     opens com.example.aerolineascanary7islands.models to org.hibernate.orm.core;
 
-    opens com.example.aerolineascanary7islands.controllers to javafx.fxml;
 }
