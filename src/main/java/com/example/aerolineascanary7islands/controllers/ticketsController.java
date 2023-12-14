6243e793845d5572d418abc6d47df3b20424bd88
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2023-12-14 01:48:07 +0100

    FUNCIONA EL CARRUSEL

--- src/main/java/com/example/aerolineascanary7islands/controllers/ticketsController.java
+++ src/main/java/com/example/aerolineascanary7islands/controllers/ticketsController.java
@@ -1,8 +1,10 @@
 package com.example.aerolineascanary7islands.controllers;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
@@ -16,20 +18,44 @@ public class ticketsController {
     @FXML
     private StackPane carrousel;
 
-    public void initialize(){
-        List<ImageView> images = new ArrayList<>();
+    @FXML
+    private ImageView imageView1;
+
+    @FXML
+    private ImageView imageView2;
+
+    @FXML
+    private Label granCanariaLabel;
+
+    @FXML
+    private Label tenerifeLabel;
+
+    private int indice = 0;
+
+    public void initialize() {
 
 
-        // Agregar todas las imágenes al StackPane
-        carrousel.getChildren().addAll(images);
+        // Inicializar la animación para cambiar las imágenes
+        Duration duration = Duration.seconds(10); // Cambia cada 3 segundos (puedes ajustar este valor)
+        Timeline timeline = new Timeline(new KeyFrame(duration, e -> cambiarImagen()));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
 
-// Configurar la transición de desplazamiento
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), carrousel);
-        transition.setByX(-200); // Ajusta el valor para el desplazamiento horizontal
-        transition.setCycleCount(Timeline.INDEFINITE); // Repetir la transición infinitamente
+    }
 
-// Reproducir la transición
-        transition.play();
-
+    private void cambiarImagen() {
+        if (indice == 0) {
+            imageView1.setVisible(false);
+            imageView2.setVisible(true);
+            granCanariaLabel.setVisible(false);
+            tenerifeLabel.setVisible(true);
+            indice = 1;
+        } else {
+            imageView2.setVisible(false);
+            imageView1.setVisible(true);
+            granCanariaLabel.setVisible(true);
+            tenerifeLabel.setVisible(false);
+            indice = 0;
+        }
     }
 }
