8c3fbfe497b311d47545abd69e533ab486b2bf10
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-12-14 19:23:47 +0000

    algunos cambios de vistas

--- src/main/java/com/example/aerolineascanary7islands/controllers/VuelosController.java
+++ src/main/java/com/example/aerolineascanary7islands/controllers/VuelosController.java
@@ -1,22 +1,26 @@
 package com.example.aerolineascanary7islands.controllers;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
-public class VuelosController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VuelosController implements Initializable {
     public Label panelControlDeVuelos;
     public TableView tablaVuelos;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
     public void crearVuelos(MouseEvent mouseEvent) {
     }
-
     public void eliminarVuelos(MouseEvent mouseEvent) {
     }
-
     public void actualizarVuelos(MouseEvent mouseEvent) {
     }
-
     public void mostrarVuelos(MouseEvent mouseEvent) {
     }
 }
